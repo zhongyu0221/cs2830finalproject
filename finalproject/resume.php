86c428a2c8ed85228a132b659b87527dc8056038
zhongyu0221 <zhongyu0221@gmail.com>
2016-04-11 23:57:51 -0500

    final

---
+++ finalproject/resume.php
@@ -0,0 +1,289 @@
+<!DOCTYPE html>
+<html lang="zh-cn">
+
+    <style>
+    
+        .aboutme.timeline.skills.contactme{
+            padding: 300px;
+            margin:300px;
+        }
+    
+    
+    
+    </style>
+    
+    <head>
+        <meta charset="UTF-8">
+          <link rel="stylesheet" href="css/bootstrap.min.css" rel="stylesheet" >
+         <link href="timeline.css"  rel="stylesheet">
+         <!-- <link href="pace.css" rel="stylesheet" type="text/css">-->
+        <link href="responsice.css" rel="stylesheet">
+        <link href="own.css" >
+        
+
+    <title> Zhongyu Li' Resume </title>
+    </head>
+    
+<body>
+    
+   <!--for text  <button class="btn btn-info">Bootstrap</button>-->
+
+    <h1 id="title">Zhongyu Li' Resume </h1>
+    
+    <!--header section-->
+    <ul class="nav nav-pills">
+        <li class="active"><a href="#aboutme" >About Me</a></li>
+        <li><a href="#timeline" >Time Line</a></li>
+        <li><a href="#skills" >Skills</a></li>
+        <li><a href="#contactme" >Contact Me</a></li>
+    </ul>
+    
+    
+      <!--header section-->
+    
+    
+    
+    <!--aboutme section-->
+        <div id="aboutme">
+            <div class=".container"><!--定义一个容器-->
+                <div class = "row">
+                <div class="col-md-4 col-md-offset-1"> 
+                   <!-- <h1>Zhongyu Li</h1>-->
+                        <div class="media">
+                            <img class="media-object img-circle img-responsive" src="IMG_4203.JPG" >                    
+                        </div>
+                    <h3><a href="Resume zhongyu li.docx" >My resume</a></h3>
+                    
+                    </div>
+                 <div class="col-md-5 col-md-offset-1" id="summary"> 
+    <p> My name is Zhongyu Li. I am a senior student in bioengineering. After finished two years’ study in China, I transferred to Mizzou as a junior student. I took 52 credit hours in MIZZOU so far and got cumulative GPA 3.89. As an engineering student, I believe computer science is an important tool to all engineers. In order to improve my ability in computer area, I took fundamental classes and study a minor degree. Because of the academic achievements, I got a position in Dean's high honor roll for the past 4 semester.</p>
+                     
+                     
+ 	<p>   I work as a research assistant with Dr. Sengupta for almost two years. My first project is about the rapid culture-based detection of mycobacteria using multi-frequency impedance measurements, which can help reducing the time-to-detection of tuberculosis and make benefits to human. Worked with two graduates and two undergraduates, I getting lots of help from them. In 2015 Spring Undergraduate Research and Creative Achievements Forum, I got the Chancellor's Awards for Excellence in Undergraduate Research in the Engineering Sciences category. Now I am working in an individual project called "Modeling the Effect of Bacterial Load on Minimum Inhibitory Concentration(MIC)". This model can be used in prediction the bacteria's concentration after over times, which can be used both in laboratory of clinical application. </p>
+                     
+                     
+   <p>  I also work as a tutor in A Way With Works&Number since this semester. A3W&N is a community-based non-profit program that teams undergraduate students from the University of Missouri (MU) with local children to improve their struggling literacy and mathematics skills. I spend more than 5 hours per week to help children in high school. This volunteer experience makes me full and better involved in American society.</p>
+                     </div>
+                    </div>
+        </div>
+    </div>
+       <!--aboutme section-->
+    
+    
+    
+    
+    
+                      <!--timeline section-->
+<div id="timeline">
+                <div class="information clearfix">
+
+                    <!--left-->
+                    <div class="info_area info_left info_content">
+
+                        <h5>My Story</h5>
+                       
+                        <!-- s1~s5 为对应右侧 时间点 -->
+                        <p class="desc s1"><em class="count">09</em>Enter Jiangnan University...</p>
+                        <p class="desc s2"><em class="count">10</em>
+                            2015 SPRING 2015 FALL Curator's Grant-in-Aid Scholarship
+                            2015 Chancellor’s Awards for Excellence in 2015 spring Undergraduate Research and Creative Achievements Forum
+                            2014 FALL  2015 SPRING 2015 FALL Dean List (Dean’s High Honor Roll)
+                        </p>
+                       
+                        <p class="desc s3"><em class="count">12</em>
+                            Working alongside graduated students
+                            Rapid detection of tuberculosis(TB) mycobacterial in clinical samples
+                            Demonstrate Electrochemical Impedance Spectroscopy based detection methodology to reduce the time-to-detection(TTD). 
+                            Preparing bacterial media of solid agar and broth solutions
+                            Ensured sterile conditions by autoclaving
+                            Independently followed instructions using various equipment 
+</p>
+                        <p class="desc s4"><em class="count">13</em>
+                            community-based non-profit program
+                            Help local children to improve their struggling literacy and mathematics skills
+</p>
+                        
+
+                        
+                    </div>
+
+                    <!--right-->
+                    <div class="info_area info_right info_timeline">
+                        <h5><i aria-hidden="true" data-icon="&#x7d;"></i>Time Line</h5>
+                        <ul>
+                            <li class="clearfix top" id="s5">
+                                <!--list样式-->
+                                <div class="dot"><b></b></div>
+                                <!--内容-->
+                                <div class="con">
+                                    <h3>Jiangnan University</h3>
+                                    <p>Bachelor of Biological Engineering</p>
+                                    <p class="desc">August 2012-May 2014</p>
+                                </div>
+                            </li>
+                            <li class="clearfix" id="s4">
+                                <!--list样式-->
+                                <div class="dot"><b></b></div>
+                                <!--内容-->
+                                <div class="con">
+                                    <h3>University of Missouri, Columbia, MO</h3>
+                                    <p> Major: Bachelor of Bioengineering(BioMed)
+                                        Minor: Computer Science
+</p>
+                                    <p class="desc">2011.1 - 2012.1</p>
+                                </div>
+                            </li>
+                            <li class="clearfix" id="s3">
+                                <!--list样式-->
+                                <div class="dot"><b></b></div>
+                                <!--内容-->
+                                <div class="con">
+                                    <h3>Dr.Sengupta’s microfluidic lab    </h3>
+                                    <p>Research Assistance</p>
+                                    <p class="desc">2012.1 - 2013.1</p>
+                                </div>
+                            </li>
+                            <li class="clearfix" id="s2">
+                                <!--list样式-->
+                                <div class="dot"><b></b></div>
+                                <!--内容-->
+                                <div class="con">
+                                    <h3>A Way With Words And Numbers</h3>
+                                    <p>Tuto</p>
+                                    <p class="desc">2014.1 - 2015.1</p>
+                                </div>
+                            </li>
+                            
+                        </ul>
+                    </div>
+    </div>
+    </div>
+                     
+         <!--timeline section-->
+    
+    
+    
+    
+    
+    
+     <!--skills section-->
+        
+        <div id= "skills">
+            <h5>My Skills</h5>
+            
+<div class="progress progress-striped">
+    <div class="progress-bar progress-bar-success" role="progressbar" 
+      aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" 	
+      style="width: 80%;"> <!--how long the bar goes-->
+      <span class="sr-only">90% 完成（成功）</span>
+   </div>
+   <span class="progress-type">  C</span>
+</div>
+            
+<div class="progress progress-striped">
+   <div class="progress-bar progress-bar-info" role="progressbar"
+      aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" 	
+      style="width: 30%;">
+      <span class="sr-only">30% 完成（信息）</span>
+   </div>
+    <span class="progress-type">  HTML</span>
+</div>
+            
+<div class="progress progress-striped">
+   <div class="progress-bar progress-bar-warning" role="progressbar" 
+      aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" 	
+      style="width: 20%;">
+      <span class="sr-only">20% 完成（警告）</span>
+   </div>
+    <span class="progress-type">  PHP</span>
+</div>
+            
+<div class="progress progress-striped">
+   <div class="progress-bar progress-bar-danger" role="progressbar" 
+      aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" 	
+      style="width: 10%;">
+      <span class="sr-only">10% 完成（危险）</span>
+   </div>
+    <span class="progress-type">  Java</span>
+</div>
+            
+            <div class="progress progress-striped">
+   <div class="progress-bar progress-bar-danger" role="progressbar" 
+      aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" 	
+      style="width: 10%;">
+      <span class="sr-only">10% 完成（危险）</span>
+   </div>
+    <span class="progress-type">  Java</span>
+</div>
+            
+            <div class="progress progress-striped">
+   <div class="progress-bar progress-bar-danger" role="progressbar" 
+      aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" 	
+      style="width: 10%;">
+      <span class="sr-only">10% 完成（危险）</span>
+   </div>
+    <span class="progress-type">  Java</span>
+</div>
+            
+            <div class="progress progress-striped">
+   <div class="progress-bar progress-bar-danger" role="progressbar" 
+      aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" 	
+      style="width: 10%;">
+      <span class="sr-only">10% 完成（危险）</span>
+   </div>
+    <span class="progress-type">  Java</span>
+</div>
+        
+        </div>
+        
+        <!--skills section-->
+    
+        
+    <!--contactme section-->
+        <div id="contactme">
+    
+      <!--contactme section-->
+    <form class="form-horizontal" role="form" method="post" action="index.php">
+        <h5>Contact me</h5>
+	<div class="form-group">
+		<label for="name" class="col-sm-2 control-label">Name</label>
+		<div class="col-sm-10">
+			<input type="text" class="form-control" id="name" name="name" placeholder="First & Last Name" value="">
+		</div>
+	</div>
+	<div class="form-group">
+		<label for="email" class="col-sm-2 control-label">Email</label>
+		<div class="col-sm-10">
+			<input type="email" class="form-control" id="email" name="email" placeholder="example@domain.com" value="">
+		</div>
+	</div>
+	<div class="form-group">
+		<label for="message" class="col-sm-2 control-label">Message</label>
+		<div class="col-sm-10">
+			<textarea class="form-control" rows="4" name="message"></textarea>
+		</div>
+	</div>
+	<!--<div class="form-group">
+		<label for="human" class="col-sm-2 control-label">2 + 3 = ?</label>
+		<div class="col-sm-10">
+			<input type="text" class="form-control" id="human" name="human" placeholder="Your Answer">
+		</div>
+	</div>-->
+	<div class="form-group">
+		<div class="col-sm-10 col-sm-offset-2">
+			<input id="submit" name="submit" type="submit" value="Send" class="btn btn-primary">
+		</div>
+	</div>
+	<div class="form-group">
+		<div class="col-sm-10 col-sm-offset-2">
+			<! Will be used to display an alert to the user>
+		</div>
+	</div>
+</form>
+    </div>
+</body>
+</html>
+    
+
+
+
