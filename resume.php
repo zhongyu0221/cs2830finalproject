5826fde6431e3935cd01bfc5bdb679d20ac142fb
zhongyu0221 <zhongyu0221@gmail.com>
2016-04-16 14:03:53 -0500

    Added files via upload

---
+++ resume.php
@@ -0,0 +1,465 @@
+<!DOCTYPE html>
+<html lang="zh-cn">
+
+    
+    <head>
+        <meta charset="UTF-8">
+          <link href="css/bootstrap.min.css" rel="stylesheet" >
+        <link href="font-awesome-4.6.1/css/font-awesome.min.css" rel="stylesheet">
+         <link href="css/timeline.css"  rel="stylesheet">
+        <link href="css/responsice.css" rel="stylesheet">
+        <link href="css/login.css" rel="stylesheet">
+        
+  
+       
+
+    <title> Zhongyu Li' Resume </title>
+    </head>
+    
+<body>
+    <style>
+        #header{
+             background-color:#EAFAF1;
+        }
+        
+        .breadcrumb
+        {
+            font-size: 20px;
+            color:white;
+           
+            text-align: center;
+        }
+        #title{
+            text-align: center;
+           
+            
+        }
+        .nav{
+            text-align: center;
+            padding:40px;
+        }
+        
+        .media{
+            
+            width:350px;
+            height:500px;
+        }
+        
+        #aboutme{
+            
+        padding:40px;
+            background-color:#D5F5E3;
+        }
+        
+        #timeline{
+        padding:40px;
+            background-color:#ABEEC6;
+        }
+        
+        .info_area h5{
+        font-size: 30px;
+        }
+        
+        #skills{
+            background-color: #82E0AA;
+             padding:40px;
+        }
+        
+        #skills h5{
+            font-size: 30px;
+        }
+        
+        #contactme h5{
+            font-size: 30px;
+        }
+        #contactme {
+            background-color:#58d68d;
+             padding:40px;
+        }
+   
+        .social_links
+        {
+            background-color:#2ecc71;
+            font-size: 20px;
+            color:gray;
+        }
+        
+        
+        #loginarea h5{
+            font-size:30px;
+            
+        }
+        
+        #loginarea  {
+        padding: 40px;
+            background-color:#28b463;
+		
+	}
+	   #stage {
+		border: 1px solid #ccc;
+		font-size: 20pt;
+    
+    </style>
+    
+   
+<div id="header">
+
+    <h1 id="title">Zhongyu Li' Resume </h1>
+    
+    <!--header section-->
+  <ol class="breadcrumb">
+        <li class="active"><a href="#aboutme" >About Me</a></li>
+        <li><a href="#timeline" >Time Line</a></li>
+        <li><a href="#skills" >Skills</a></li>
+        <li><a href="#contactme" >Contact Me</a></li>
+         <li><a href="#login" >Log in</a></li>
+    </ol>
+    
+    </div>
+      <!--header section-->
+    
+    
+    
+    <!--aboutme section-->
+        <div id="aboutme">
+            <div class="container"><!--定义一个容器-->
+                <div class = "row">
+                <div class="col-md-4 col-md-offset-1"> 
+                  
+                        <div class="media">
+                            <img class="media-object img-circle img-responsive" src="IMG_4203.JPG" >                    
+                        </div>
+                  <!--  <h3><a href="Resume zhongyu li.docx" >My resume</a></h3>-->
+                    
+                    </div>
+                 <div class="col-md-5 col-md-offset-1" id="summary"> 
+    <p> My name is Zhongyu Li. I am a senior student in bioengineering. After finished two years’ study in China, I transferred to Mizzou as a junior student. I took 52 credit hours in MIZZOU so far and got cumulative GPA 3.89. As an engineering student, I believe computer science is an important tool to all engineers. In order to improve my ability in computer area, I took fundamental classes and study a minor degree. Because of the academic achievements, I got a position in Dean's high honor roll for the past 4 semester.</p>
+                     
+                     
+ 	<p>   I work as a research assistant with Dr. Sengupta for almost two years. My first project is about the rapid culture-based detection of mycobacteria using multi-frequency impedance measurements, which can help reducing the time-to-detection of tuberculosis and make benefits to human. Worked with two graduates and two undergraduates, I getting lots of help from them. In 2015 Spring Undergraduate Research and Creative Achievements Forum, I got the Chancellor's Awards for Excellence in Undergraduate Research in the Engineering Sciences category. Now I am working in an individual project called "Modeling the Effect of Bacterial Load on Minimum Inhibitory Concentration(MIC)". This model can be used in prediction the bacteria's concentration after over times, which can be used both in laboratory of clinical application. </p>
+                     
+                     
+   <p>  I also work as a tutor in A Way With Works&Number since this semester. A3W&N is a community-based non-profit program that teams undergraduate students from the University of Missouri (MU) with local children to improve their struggling literacy and mathematics skills. I spend more than 5 hours per week to help children in high school. This volunteer experience makes me full and better involved in American society.</p>
+                     </div>
+                    </div>
+        </div>
+    </div>
+       <!--aboutme section-->
+    
+    
+    
+    
+    
+                      <!--timeline section-->
+<div id="timeline">
+                <div class="information clearfix">
+
+                    <!--left-->
+                    <div class="info_area info_left info_content">
+
+                        <h5>My Story</h5>
+                       
+                        <!-- s1~s5 为对应右侧 时间点 -->
+                        <p class="desc s1"> Enter Jiangnan University...</p>
+                        <p></p>
+                        <p></p>
+                        <p></p>
+                        <p></p>
+                        <p></p>
+                        <p></p>
+                        <p class="desc s2">
+                            2015 SPRING 2015 FALL Curator's Grant-in-Aid Scholarship
+                            2015 Chancellor’s Awards for Excellence in 2015 spring Undergraduate Research and Creative Achievements Forum
+                            2014 FALL  2015 SPRING 2015 FALL Dean List (Dean’s High Honor Roll)
+                        </p>
+                       <p></p>
+                        <p></p>
+                        <p class="desc s3">
+                            Working alongside graduated students
+                            Rapid detection of tuberculosis(TB) mycobacterial in clinical samples
+                            Demonstrate Electrochemical Impedance Spectroscopy based detection methodology to reduce the time-to-detection(TTD). 
+                            Preparing bacterial media of solid agar and broth solutions
+                            Ensured sterile conditions by autoclaving
+                            Independently followed instructions using various equipment 
+</p>                <p></p>
+                        <p></p>
+                       
+                        <p class="desc s4">
+                            community-based non-profit program
+                            Help local children to improve their struggling literacy and mathematics skills
+</p>
+                        
+
+                        
+                    </div>
+
+                    <!--right-->
+                    <div class="info_area info_right info_timeline">
+                        <h5>Time Line</h5>
+                        <ul>
+                            <li class="clearfix top" id="s5">
+                                <!--list样式-->
+                                <div class="dot"><b></b></div>
+                                <!--内容-->
+                                <div class="con">
+                                    <h3>Jiangnan University</h3>
+                                    <p>Bachelor of Biological Engineering</p>
+                                    <p class="desc">August 2012-May 2014</p>
+                                </div>
+                            </li>
+                            <li class="clearfix" id="s4">
+                                <!--list样式-->
+                                <div class="dot"><b></b></div>
+                                <!--内容-->
+                                <div class="con">
+                                    <h3>University of Missouri, Columbia, MO</h3>
+                                    <p> Major: Bachelor of Bioengineering(BioMed)
+                                        Minor: Computer Science
+</p>
+                                    <p class="desc">2011.1 - 2012.1</p>
+                                </div>
+                            </li>
+                            <li class="clearfix" id="s3">
+                                <!--list样式-->
+                                <div class="dot"><b></b></div>
+                                <!--内容-->
+                                <div class="con">
+                                    <h3>Dr.Sengupta’s microfluidic lab    </h3>
+                                    <p>Research Assistance</p>
+                                    <p class="desc">2012.1 - 2013.1</p>
+                                </div>
+                            </li>
+                            <li class="clearfix" id="s2">
+                                <!--list样式-->
+                                <div class="dot"><b></b></div>
+                                <!--内容-->
+                                <div class="con">
+                                    <h3>A Way With Words And Numbers</h3>
+                                    <p>Tuto</p>
+                                    <p class="desc">2014.1 - 2015.1</p>
+                                </div>
+                            </li>
+                            
+                        </ul>
+                    </div>
+    </div>
+    </div>
+                     
+         <!--timeline section-->
+    
+    
+    
+    
+    
+    
+     <!--skills section-->
+        
+        <div id= "skills">
+            <h5>My Skills</h5>
+            
+<div class="progress progress-striped">
+    <div class="progress-bar progress-bar-success" role="progressbar" 
+      aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" 	
+      style="width: 70%;"> <!--how long the bar goes-->
+      <span class="sr-only">90% 完成（成功）</span>
+   </div>
+   <span class="progress-type">  Research</span>
+</div>
+            
+<div class="progress progress-striped">
+   <div class="progress-bar progress-bar-info" role="progressbar"
+      aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" 	
+      style="width: 60%;">
+      <span class="sr-only">30% 完成（信息）</span>
+   </div>
+    <span class="progress-type">  C</span>
+</div>
+            
+<div class="progress progress-striped">
+   <div class="progress-bar progress-bar-warning" role="progressbar" 
+      aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" 	
+      style="width: 40%;">
+      <span class="sr-only">20% 完成（警告）</span>
+   </div>
+    <span class="progress-type">  HTML</span>
+</div>
+            
+<div class="progress progress-striped">
+   <div class="progress-bar progress-bar-danger" role="progressbar" 
+      aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" 	
+      style="width: 10%;">
+      <span class="sr-only">10% 完成（危险）</span>
+   </div>
+    <span class="progress-type">  Java</span>
+</div>
+            
+            <div class="progress progress-striped">
+   <div class="progress-bar progress-bar-info" role="progressbar" 
+      aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" 	
+      style="width: 60%;">
+      <span class="sr-only">10% 完成（危险）</span>
+   </div>
+    <span class="progress-type">  Bacterial</span>
+</div>
+            
+            <div class="progress progress-striped">
+   <div class="progress-bar progress-bar-success" role="progressbar" 
+      aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" 	
+      style="width: 85%;">
+      <span class="sr-only">10% 完成（危险）</span>
+   </div>
+    <span class="progress-type">  Lab Work</span>
+</div>
+            
+            
+        
+        </div>
+        
+        <!--skills section-->
+    
+        
+    <!--contactme section-->
+        <div id="contactme">
+    
+
+    <form class="form-horizontal" role="form" method="post" action="index.php">
+        <h5>Contact me</h5>
+	<div class="form-group">
+		<label for="name" class="col-sm-2 control-label">Name</label>
+		<div class="col-sm-10">
+			<input type="text" class="form-control" id="name" name="name" placeholder="First & Last Name" value="">
+		</div>
+	</div>
+	<div class="form-group">
+		<label for="email" class="col-sm-2 control-label">Email</label>
+		<div class="col-sm-10">
+			<input type="email" class="form-control" id="email" name="email" placeholder="example@domain.com" value="">
+		</div>
+	</div>
+	<div class="form-group">
+		<label for="message" class="col-sm-2 control-label">Message</label>
+		<div class="col-sm-10">
+			<textarea class="form-control" rows="4" name="message"></textarea>
+		</div>
+	</div>
+	<!--<div class="form-group">
+		<label for="human" class="col-sm-2 control-label">2 + 3 = ?</label>
+		<div class="col-sm-10">
+			<input type="text" class="form-control" id="human" name="human" placeholder="Your Answer">
+		</div>
+	</div>-->
+	<div class="form-group">
+		<div class="col-sm-10 col-sm-offset-2">
+			<input id="submit" name="submit" type="submit" value="Send" class="btn btn-primary">
+		</div>
+	</div>
+	<div class="form-group">
+		<div class="col-sm-10 col-sm-offset-2">
+			<! Will be used to display an alert to the user>
+		</div>
+	</div>
+</form>
+            
+            
+
+    
+    
+    
+   
+  
+          <ul class="social_links">
+              
+<p></p>
+<p></p>
+               <li class=""><i class="fa fa-phone-square"></i>  651-353-6750</li>
+              
+               <li class=""><i class="fa fa-envelope"></i>  Zhongyu0221@gmail.com</li>
+              
+            <li class=""><a href="https://github.com/zhongyu0221" target="https://github.com/zhongyu0221"><i class="fa fa-github"></i>&nbsp;</a>    Github</li><!--target 打开一个新的窗口-->
+            
+            <li class=""><a href="https://www.linkedin.com" target="https://www.linkedin.com"><i class="fa fa-linkedin"></i>&nbsp;</a>  Linkedin</li>
+        
+              
+            <li class=""><a href="https://www.facebook.com/zhongyu0221" target ="https://www.facebook.com/zhongyu0221"><i class="fa fa-facebook"></i></a>   Facebook</li>
+      
+            <li class=""><a href="https://www.facebook.com/zhongyu0221" target ="https://www.facebook.com/zhongyu0221"><i class="fa fa-weixin"></i></a> zooooooooooe</li>
+
+              
+          </ul>
+    </div>
+            
+    
+    <!--log in section-->
+    <div id="loginarea">
+        
+    <h5>Log in</h5>
+        
+             
+	<div id="stage"></div>
+        
+        
+	<input type="text" id="username">
+	<input type="password" id="password">
+	<button id="login">Login</button>
+	<button id="logout">Logout</button>
+	<button id="get">Get Content</button>
+        
+ 
+</div>
+
+        
+	<script src="js/jquery-1.10.2.min.js"></script>
+        
+	<script>
+	$(function(){
+		$('#login').click(function(){
+			$.post('js/process.php', 
+			{
+				action: 'login',
+				username: $('#username').val(),
+				password: $('#password').val()
+			},
+                   
+			function(data){
+				$('#stage').html(data);
+			});
+		});
+        
+        
+		$('#logout').click(function(){
+			$.post('js/process.php',
+			{
+				action: 'logout'
+			},
+			function(data){
+				$('#stage').html(data);
+			});
+		});
+        
+        
+		$('#get').click(function(){
+			$.post('js/process.php',
+			{
+				action: 'get'
+			},
+			function(data){
+				$('#stage').html(data);
+			});
+		});
+	});
+	</script>
+
+       
+	
+<!--log in section-->
+    
+</body>
+    
+    
+    
+    
+</html>
+    
+
+
+
